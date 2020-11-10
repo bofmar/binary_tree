f226651332e61f00b235537496e7ffef4ca34179
bofmar <bofmar@outlook.com>
2020-11-10 16:27:57 +0200

    initial for delete function

--- binary_tree.rb
+++ binary_tree.rb
@@ -36,12 +36,18 @@ class Tree
   end
 
   def delete value
-    _delete value
+    @root = _delete value
   end
 
   def find value
     result = _find value, root
     return (result.value == value) ? result : nil
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
   private
@@ -111,22 +117,94 @@ class Tree
       p "No such node exists. Operation failed successfuly"
       return root
     else
-      parent = _find_parent value, root
-      p parent.left.value
-      p parent.right.value
-      p parent.value
+      if root.value == value # TO DO: FIX THIS, IT IS WRONG
+        parent = root
+        if root.right == nil && root.left == nil
+          return nil
+        end
+      else
+        parent = _find_parent value, root
+      end
+      if parent.value > value
+        child = parent.left
+      else
+        child = parent.right
+      end
+      if child.left == nil && child.right == nil # we are deleting a leaf node
+        if parent.value > value
+          parent.left = nil
+        else
+          parent.right = nil
+        end
+      elsif child.left == nil && child.right != nil || child.right == nil && child.left != nil #only one grandchild exists
+        if parent.value > value
+          if child.left == nil
+            grandchild = child.right
+            child.right = nil
+            parent.left = grandchild
+          else
+            grandchild = child.left
+            child.left = nil
+            parent.left = grandchild
+          end
+        else
+          if child.left == nil
+            grandchild = child.right
+            child.right = nil
+            parent.right = grandchild
+          else
+            grandchild = child.left
+            child.left = nil
+            parent.right = grandchild
+          end
+        end
+      else # two grandchildren exist
+        inheritor = _find_inheritor child.right
+        grandchild = _find_parent inheritor.value, root
+
+        if grandchild == child #the node we are replacing is the same as the parent of the inheritor
+          if parent.value > value
+            parent.left = inheritor
+          else
+            parent.right = inheritor
+          end
+          child.right = nil
+          inheritor.left = child.left
+          child.left = nil
+        else
+          if parent.value > value
+            parent.left = inheritor
+          else
+            parent.right = inheritor
+          end
+          grandchild.left = inheritor.right
+          inheritor.left = child.left
+          inheritor.right = child.right
+          child.left = nil
+          child.right = nil
+        end
+      end
     end
+    return root
   end
 
   def _find_parent value, node
-    if node.left.value == value || node.right.value == value
+    if node.left != nil && node.left.value == value || node.right != nil && node.right.value == value
       return node
     elsif node.value > value
       node = _find_parent value,node.left
     else
       node = _find_parent value,node.right
     end
+    return node
+  end
 
+  def _find_inheritor node
+    if node.left == nil
+      return node
+    else
+      node = _find_inheritor node.left
+    end
     return node
   end
 end
@@ -137,4 +215,4 @@ test_arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 tree = Tree.new test_arr
 testNode.left = Node.new(6)
 
-tree.delete 324
+tree.pretty_print
